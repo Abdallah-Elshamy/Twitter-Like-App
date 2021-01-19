d1874e10a1af54a3a5a6e5f9da9d9b587d7831c7
omar <omarabdo997@gmail.com>
2021-01-19 17:53:17 +0200

    [TWIT-25] add Likes model

---
+++ backend/src/models/likes.ts
@@ -0,0 +1,26 @@
+import {
+    Table,
+    Column,
+    Model,
+    ForeignKey,
+    PrimaryKey,
+    DataType,
+} from 'sequelize-typescript';
+import User from './user'
+import Tweet from './tweet'
+
+@Table({
+    tableName: 'likes'
+})
+class Likes extends Model {
+    @PrimaryKey
+    @ForeignKey(() => User)
+    @Column(DataType.INTEGER)
+    userId!: number;
+
+    @PrimaryKey
+    @ForeignKey(() => Tweet)
+    @Column(DataType.INTEGER)
+    tweetId!: number;
+}
+

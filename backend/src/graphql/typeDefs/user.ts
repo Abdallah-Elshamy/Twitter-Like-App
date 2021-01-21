26d690d3b06f76cf81d3e8387632778a649406fb
omar <omarabdo997@gmail.com>
2021-01-21 16:53:39 +0200

    [TWIT-40]add user typeDefs

--- backend/src/graphql/typeDefs/user.ts
+++ backend/src/graphql/typeDefs/user.ts
@@ -1,7 +1,52 @@
-import { gql } from 'apollo-server-express';
+import { gql } from "apollo-server-express";
 
-export default gql `
+export default gql`
     extend type Query {
         bye: String!
+    }
+
+    type User {
+    id: ID!
+    userName: String!
+    email: String!
+    name: String!
+    imageURL: String
+    bio: String
+    coverImageURL: String
+    following(page: Int): PaginatedUsers!
+    followingCount: Int!
+    followers(page: Int): PaginatedUsers!
+    followersCount: Int!,
+    tweets(page: Int): PaginatedTweets!
+    likes(page: Int): PaginatedTweets!
+    groups: [String]!
+    permissions: [String]!
+    createdAt: String!
+    updatedAt: String!
+    }
+
+    type PaginatedUsers {
+    total_count: Int!
+    users: [User]!
+    }
+
+    input UserCreateInput {
+    userName: String!
+    email: String!
+    password: String!
+    name: String!
+    imageURL: String
+    bio: String
+    coverImageURL: String
+    }
+
+    input UserUpdateInput {
+    userName: String
+    email: String
+    password: String
+    name: String
+    imageURL: String
+    bio: String
+    coverImageURL: String
     }
 `;

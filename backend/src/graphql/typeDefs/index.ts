8d73a167813208a78c58783d4859474bcc82b43d
omar <omarabdo997@gmail.com>
2021-01-21 17:21:40 +0200

    [TWIT-40] remove post typeDef and resolver from schema files

--- backend/src/graphql/typeDefs/index.ts
+++ backend/src/graphql/typeDefs/index.ts
@@ -1,7 +1,6 @@
 import base from "./base";
-import post from "./post";
 import tweet from "./tweet";
 import user from "./user";
 import hashtag from "./hashtag";
 
-export default [base, post, user, tweet, hashtag];
+export default [base, user, tweet, hashtag];

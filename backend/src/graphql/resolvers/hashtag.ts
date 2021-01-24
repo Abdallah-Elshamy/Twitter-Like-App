24b5080a2bc26b32c12fbd5531b8f70c31a7124d
Omar Fathy <o_fathy28@yahoo.com>
2021-01-24 15:04:57 +0200

    [TWIT-47] add hashtag query in resolvers

--- backend/src/graphql/resolvers/hashtag.ts
+++ backend/src/graphql/resolvers/hashtag.ts
@@ -1,3 +1,49 @@
+import { Hashtag } from "../../models";
+import validator from "validator";
+
+const PAGE_SIZE = 2;
+
 export default {
-    Query: {},
+    Query: {
+        hashtag: async (parent: any, args: any, context: any, info: any) => {
+            const word = args.word;
+            if(validator.isEmpty(word)) {
+                const error: any = new Error(
+                    "Empty query argument!"
+                );
+                error.statusCode = 422;
+                console.log(error.message);
+                throw error;
+            }
+            const hashtag: any = await Hashtag.findByPk(word);
+            if (!hashtag) {
+                const error: any = new Error(
+                    "No hashtag found with this word!"
+                );
+                error.statusCode = 404;
+                console.log(error.message);
+                throw error;
+            }
+
+            return hashtag;
+        },
+    },
+    Hashtag: {
+        tweets: async (parent: any, args: any) => {
+            const { page } = args;
+            return {
+                tweets: async () => {
+                    const tweets = await parent.$get("tweets", {
+                        offset: (page - 1) * PAGE_SIZE || 0,
+                        limit: page ? PAGE_SIZE : undefined,
+                    });
+                    return tweets;
+                },
+                totalCount: async () => {
+                    const count = await parent.$count("tweets");
+                    return count;
+                },
+            };
+        },
+    },
 };

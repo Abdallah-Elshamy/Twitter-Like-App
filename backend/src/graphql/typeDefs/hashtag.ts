063ef7fa2ccbf40f215a519ab99e946ab824d269
Omar Fathy <o_fathy28@yahoo.com>
2021-01-24 14:26:23 +0200

    [TWIT-47] change name to word in type Hashtag

--- backend/src/graphql/typeDefs/hashtag.ts
+++ backend/src/graphql/typeDefs/hashtag.ts
@@ -2,7 +2,7 @@ import { gql } from "apollo-server-express";
 
 export default gql`
     type Hashtag {
-        name: String!
+        word: String!
         tweets(page: Int): PaginatedTweets
         createdAt: String
         updatedAt: String

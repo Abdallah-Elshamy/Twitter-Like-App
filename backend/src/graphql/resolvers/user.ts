b05716c345c39affe65d0f5133a7e7f3357ecff7
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-01-26 19:17:32 +0200

    Arrange user resolver functions

--- backend/src/graphql/resolvers/user.ts
+++ backend/src/graphql/resolvers/user.ts
@@ -9,52 +9,36 @@ const PAGE_SIZE = 10;
 export default {
     Query: {},
     Mutation: {
-        unlike: async (parent: any, args: any, context: any, info: any) => {
-            // assume that the loggedin user has an id of 1
-            const currentUser: any = await User.findByPk(1);
-
-            const tweet: any = await Tweet.findByPk(args.tweetId);
-            if (!tweet) {
-                const error: any = new Error("No tweet found with this id");
-                error.statusCode = 404;
-                throw error;
+        createUser: async (parent: any, args: any, context: any, info: any) => {
+            const { userInput } = args;
+            const validators = UserValidator(userInput);
+            if (await User.findOne({ where: { email: userInput.email } })) {
+                validators.push({
+                    message: "This email address is already being used",
+                    value: "email",
+                });
             }
-            const isLiked = await currentUser.hasLikes(tweet);
-
-            // check if the entered tweet is liked by the current user
-            if (!isLiked) {
-                const error: any = new Error(
-                    `The current user doesn't like tweet with id ${tweet.id}`
-                );
+            if (
+                await User.findOne({ where: { userName: userInput.userName } })
+            ) {
+                validators.push({
+                    message: "This user name is already being used",
+                    value: "userName",
+                });
+            }
+            if (validators.length > 0) {
+                const error: any = new Error("Validation error!");
                 error.statusCode = 422;
+                error.validators = validators;
                 throw error;
             }
-            await currentUser.$remove("likes", tweet);
-            return true;
-        },
-        unfollow: async (parent: any, args: any, context: any, info: any) => {
-            // assume logged in user id is 1
-            const currentUser: any = await User.findByPk(1);
-
-            // check if the entered user is found in the database
-            const toBeUnfollowed: any = await User.findByPk(args.userId);
-            if (!toBeUnfollowed) {
-                const error: any = new Error("No user found with this id");
-                error.statusCode = 404;
-                throw error;
-            }
-            const isFollowing = await currentUser.hasFollowing(toBeUnfollowed);
-            // check if the current user is following the entered user
-            if (!isFollowing) {
-                const error: any = new Error(
-                    `The current user is not following the user with id ${toBeUnfollowed.id}`
-                );
-                error.statusCode = 422;
-                throw error;
-            }
-            await currentUser.$remove("following", toBeUnfollowed);
-
-            return true;
+            // TODO: Hash the password
+            userInput.hashedPassword = userInput.password;
+            delete userInput.password;
+            const user = await db.transaction(async (transaction) => {
+                return await User.create(userInput, { transaction });
+            });
+            return user;
         },
         updateUser: async (parent: any, args: any, context: any, info: any) => {
             const { id, userInput } = args;
@@ -115,36 +99,52 @@ export default {
 
             return updatedUser;
         },
-        createUser: async (parent: any, args: any, context: any, info: any) => {
-            const { userInput } = args;
-            const validators = UserValidator(userInput);
-            if (await User.findOne({ where: { email: userInput.email } })) {
-                validators.push({
-                    message: "This email address is already being used",
-                    value: "email",
-                });
-            }
-            if (
-                await User.findOne({ where: { userName: userInput.userName } })
-            ) {
-                validators.push({
-                    message: "This user name is already being used",
-                    value: "userName",
-                });
-            }
-            if (validators.length > 0) {
-                const error: any = new Error("Validation error!");
-                error.statusCode = 422;
-                error.validators = validators;
+        like: async (parent: any, args: any, context: any, info: any) => {
+            // assume that the logged in user has an id of 1
+            const currentUser: any = await User.findByPk(1);
+
+            const tweet: any = await Tweet.findByPk(args.tweetId);
+            if (!tweet) {
+                const error: any = new Error("No tweet found with this id");
+                error.statusCode = 404;
                 throw error;
             }
-            // TODO: Hash the password
-            userInput.hashedPassword = userInput.password;
-            delete userInput.password;
-            const user = await db.transaction(async (transaction) => {
-                return await User.create(userInput, { transaction });
+            const isLiked = await currentUser.hasLikes(tweet);
+
+            // check if the entered tweet is liked by the current user
+            if (isLiked) {
+                const error: any = new Error("This tweet is already liked");
+                error.statusCode = 422;
+                throw error;
+            }
+            await db.transaction(async (transaction) => {
+                return await currentUser.$add("likes", tweet);
             });
-            return user;
+
+            return true;
+        },
+        unlike: async (parent: any, args: any, context: any, info: any) => {
+            // assume that the loggedin user has an id of 1
+            const currentUser: any = await User.findByPk(1);
+
+            const tweet: any = await Tweet.findByPk(args.tweetId);
+            if (!tweet) {
+                const error: any = new Error("No tweet found with this id");
+                error.statusCode = 404;
+                throw error;
+            }
+            const isLiked = await currentUser.hasLikes(tweet);
+
+            // check if the entered tweet is liked by the current user
+            if (!isLiked) {
+                const error: any = new Error(
+                    `The current user doesn't like tweet with id ${tweet.id}`
+                );
+                error.statusCode = 422;
+                throw error;
+            }
+            await currentUser.$remove("likes", tweet);
+            return true;
         },
         follow: async (parent: any, args: any, context: any, info: any) => {
             // assume logged in user id is 1
@@ -180,27 +180,27 @@ export default {
 
             return true;
         },
-        like: async (parent: any, args: any, context: any, info: any) => {
-            // assume that the logged in user has an id of 1
+        unfollow: async (parent: any, args: any, context: any, info: any) => {
+            // assume logged in user id is 1
             const currentUser: any = await User.findByPk(1);
 
-            const tweet: any = await Tweet.findByPk(args.tweetId);
-            if (!tweet) {
-                const error: any = new Error("No tweet found with this id");
+            // check if the entered user is found in the database
+            const toBeUnfollowed: any = await User.findByPk(args.userId);
+            if (!toBeUnfollowed) {
+                const error: any = new Error("No user found with this id");
                 error.statusCode = 404;
                 throw error;
             }
-            const isLiked = await currentUser.hasLikes(tweet);
-
-            // check if the entered tweet is liked by the current user
-            if (isLiked) {
-                const error: any = new Error("This tweet is already liked");
+            const isFollowing = await currentUser.hasFollowing(toBeUnfollowed);
+            // check if the current user is following the entered user
+            if (!isFollowing) {
+                const error: any = new Error(
+                    `The current user is not following the user with id ${toBeUnfollowed.id}`
+                );
                 error.statusCode = 422;
                 throw error;
             }
-            await db.transaction(async (transaction) => {
-                return await currentUser.$add("likes", tweet);
-            });
+            await currentUser.$remove("following", toBeUnfollowed);
 
             return true;
         },

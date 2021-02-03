2ca47dbbb6697a755c4b3f044f89dd1922dd3298
Aya Mosaad Mohamed <36517014+AyaMosaad1@users.noreply.github.com>
2021-02-02 15:07:08 -1000

    [TWIT-9]design of sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,26 @@
 import React from 'react';
+
 import './App.css';
 import './routes/Profile'
 import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import Profile from './routes/Profile';
 
-const client = new ApolloClient({
+
+
+
+const client = new ApolloClient ({
   uri: 'http://localhost:8000',
   // uri: 'http://localhost:8000/graphql',
   cache: new InMemoryCache()
 });
+
 function App() {
   return (
     <ApolloProvider client={client}>
 
-      <Profile />
-
+      <Profile/>
     </ApolloProvider>
   );
 }
-      /* routes {
-        Profile 
-        Logein
-        Signup 
-        LandingPage
-        Home 
-        User
-        Tweet
-        Explore
-        Hashtg
-      } */
+
 export default App;

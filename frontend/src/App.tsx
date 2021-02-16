32f4f6ca1b5af20d6fa05c242210edeea9bc96ee
eslam <esla.ahme@hotmail.com>
2021-02-16 10:23:43 +0200

    cannot split null token error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {ApolloProvider } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { parseJwt } from './common/utils/jwtDecoder';
@@ -8,12 +8,11 @@ import { parseJwt } from './common/utils/jwtDecoder';
 import { Routing } from './routes/routing';
 import { clientLog } from './components/Register/login_form/login';
 
-var token ;
-var  decodedToken1 ;
-if (localStorage.getItem('token') !== ' ')
-{
+var token;
+var decodedToken1;
+if (localStorage.getItem('token') !== ' ') {
   token = localStorage.getItem('token')
-  decodedToken1 = parseJwt(token)
+  token ? decodedToken1 = parseJwt(token) : decodedToken1 = null
 }
 const client = clientLog
 export const decodedToken = decodedToken1

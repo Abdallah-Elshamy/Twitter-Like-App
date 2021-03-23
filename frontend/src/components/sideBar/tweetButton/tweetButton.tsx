120f13269cdcbda1149b9f2a6fb2ac592c98bef6
eslam <esla.ahme@hotmail.com>
2021-03-23 19:15:20 +0200

    small tunning

--- frontend/src/components/sideBar/tweetButton/tweetButton.tsx
+++ frontend/src/components/sideBar/tweetButton/tweetButton.tsx
@@ -6,23 +6,23 @@ import './tweetButton.css'
 
 
 interface Button_info {
-    name: string ,
-    className? : string ,
-    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+  name: string,
+  className?: string,
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 export class TweetButton extends Component<Button_info>{
-render() {
-  return ( 
-  
-  <div> 
-    <button  className= {`mt-8 focus:outline-none h-12
-          transform transition hover:scale-110 duration-300 
-          hover:shadow-md sidebar_tw_btn ${this.props.className}`}   onClick = {this.props.onClick}  >
-          <strong className="text-center">{this.props.name}</strong>
-    </button>  
-  </div>
-)
+  render() {
+    return (
 
-}
+      <div>
+        <button className={`mt-4 focus:outline-none h-10
+          transform transition hover:scale-110 duration-300 
+          hover:shadow-md sidebar_tw_btn ${this.props.className}`} onClick={this.props.onClick}  >
+          <strong className="text-center">{this.props.name}</strong>
+        </button>
+      </div>
+    )
+
+  }
 }

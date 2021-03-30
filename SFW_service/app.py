7e0ffce4dec7d446e74cc60a3f6c4599e5ea639e
omarabdo997 <omarabdo997@gmail.com>
2021-03-30 21:27:33 +0200

    [TWIT-96] add sfw module in server

--- SFW_service/app.py
+++ SFW_service/app.py
@@ -4,7 +4,6 @@ from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
 from better_profanity import profanity
 from nudenet import NudeClassifier
-from nudenet import NudeDetector
 import base64
 import requests
 import urllib.request
@@ -12,22 +11,29 @@ import time
 import random
 import string
 
+
+def is_SFW_image(image_path):
+    result = classifier.classify(image_path)
+    if (result[image_path]["unsafe"] > 0.8):
+        return False
+    return True
+
+def is_SFW_video(video_path):
+    result = classifier.classify_video(video_path)
+    count = len(result['preds'])
+    print (f"count------------------{count}")
+    score = 0
+    for item in result['preds'].items():
+        print (f"item------------------{item}")
+        score+= item[1]['unsafe']
+    print(f"score/count---------{score/count}")    
+    if (score/count) > 0.8:
+        return False    
+    return True
+
 def create_random_string():
-    letters = string. ascii_lowercase
+    letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(10))
-
-# class HeadRequest(urllib.request):
-#         def get_method(self):
-#             return 'HEAD'
-
-# def get_contenttype(image_url):
-#     try:
-#         response= urllib.request.urlopen(HeadRequest(image_url))
-#         maintype= response.headers['Content-Type'].split(';')[0].lower()
-#         return maintype
-#     except urllib.request.HTTPError as e:
-#         print(e)
-#         return None
 
 def create_app():
     # create and configure the app
@@ -40,7 +46,7 @@ def create_app():
             'Access-Control-Allow-Headers',
             'Content-Type, Authorization, true')
         response.headers.add(
-            'Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+            'Access-Control-Allow-Methods', 'GET, POST')
         return response
 
 ###############################################################################
@@ -57,42 +63,67 @@ def create_app():
     @app.route('/safe-for-work', methods=['POST'])
     def is_SFW():
         error = 0
-        # try:
-        classifier = NudeClassifier()
-        body = request.get_json()
-        text = body.get('text', '')
-        media_urls = body.get('mediaURLs', [])
-        for url in media_urls:
-            valid = validators.url(url)
-            if not valid:
-                error = 422
-                description = 'Invalid url was supplied!'
-        if(text == '' and len(media_urls)==0):
-            error = 422
-            description = 'Please provide a text or mediaURLS in your tweet to be checked!'
-        elif(profanity.contains_profanity(text)):
-            return jsonify({
-                'success': True,
-                'SFW': False,
-                'message': 'This Tweet contains inappropriate word(s) and is marked as not safe for work!'
-            })
-        elif error == 0:
+        try:
+            body = request.get_json()
+            text = body.get('text', '')
+            media_urls = body.get('mediaURLs', [])
             for url in media_urls:
-                response = urllib.request.urlopen(url)
-                extension = response.headers['Content-Type'].split("/")[1]
-                media_name = str(time.time()) + create_random_string() + "." + extension
-                urllib.request.urlretrieve(url, media_name)
-            return jsonify({
-                'success': True,
-                'text': text,
-                'mediaURL': media_urls,
-            })
-        # except:
-        #     error = 500
-        #     description = 'Something went wrong!'
-        # finally:
-        #     if(error):
-        #         abort(error, description=description)
+                valid = validators.url(url)
+                if not valid:
+                    error = 422
+                    description = 'Invalid url was supplied!'
+            if(text == '' and len(media_urls)==0):
+                error = 422
+                description = 'Please provide a text or mediaURLS in your tweet to be checked!'
+            elif(profanity.contains_profanity(text)):
+                return jsonify({
+                    'success': True,
+                    'SFW': False,
+                    'message': 'This Tweet contains inappropriate word(s) and is marked as not safe for work!'
+                })
+            elif error == 0:
+                for url in media_urls:
+                    response = urllib.request.urlopen(url)
+                    extension = response.headers['Content-Type'].split("/")[1]
+                    media_name = str(time.time()) + create_random_string() + "." + extension
+                    media_path = f"media_files/{media_name}"
+                    urllib.request.urlretrieve(url, media_path)
+                    type_ = extension.lower()
+                    if(type_ == "jpg" or type_ == "png" or type_ == "gif" or type_ == "webp" or type_ == "tiff"
+                        or type_ == "psd" or type_ == "raw" or type_ == "bmp" or type_ == "heif" or type_ == "indd"
+                        or type_ == "jpeg"):
+                        is_SFW = is_SFW_image(media_path)
+                        os.remove(media_path)
+                        if(is_SFW):
+                            continue
+                        else:
+                            return jsonify({
+                                'success': True,
+                                'SFW': False,
+                                'message': 'Media contains an unsafe for work image!'
+                            })
+                    if(type_ == "mp4" or type_ == "mov" or type_ == "wmv" or type_ == "flv" or type_ == "avi"
+                        or type_ == "avchd" or type_ == "Webm" or type_ == "web" or type_ == "mkv"):
+                        is_SFW = is_SFW_video(media_path)
+                        os.remove(media_path)
+                        if(is_SFW):
+                            continue
+                        else:
+                            return jsonify({
+                                'success': True,
+                                'SFW': False,
+                                'message': 'Media contains an unsafe for work video!'
+                            })
+                return jsonify({
+                    'success': True,
+                    'SFW': True,
+                })
+        except:
+            error = 500
+            description = 'Something went wrong!'
+        finally:
+            if(error):
+                abort(error, description=description)
 
 ###############################################################################
 # ERRORS#######################################################################
@@ -145,4 +176,5 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
+    classifier = NudeClassifier()
     app.run(host='localhost', port=8080, debug=True)

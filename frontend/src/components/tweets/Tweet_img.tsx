0b96a22da6893efbde10a3fb99be7caa0da04327
Aya Mosaad Mohamed <36517014+AyaMosaad1@users.noreply.github.com>
2021-04-27 23:22:41 -1000

    [TWIT-123]create Tweet useInfo component

---
+++ frontend/src/components/tweets/Tweet_img.tsx
@@ -0,0 +1,18 @@
+import './tweet.css';
+import avatar from "../../routes/mjv-d5z8_400x400.jpg";
+
+
+function Tweet_img(props:any){
+
+  return(
+
+   <a href ="/user_route" className="tweet-icon mr-2">
+      {props.imageURL ? (
+        <img src={props.imageURL}
+          alt="avatar" />
+      ) : (<img src={avatar} alt="avatar" />)}
+    </a>
+
+  )}
+
+export default Tweet_img ;
